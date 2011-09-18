e11f7428c3e3d141e4503064643739b65b3a3e09
James Cline <james.cline@gatech.edu>
2011-09-17 22:36:51 -0400

    stop making requests if no more images

--- loadimages.js
+++ loadimages.js
@@ -1,17 +1,21 @@
-var current = 0
-var ids = new Object()
+var current = 0;
+var ids = new Object();
+var stop = false;
 
-var first = '<div class="singlePost"><div class="summary">'
-var second = '</div> <div class="permalinkLinks"><a href="'
-var third = '" class="highresMeta">Hi-res</a> <a href="'
-var fourth = '" class="notesMeta">'
-var fifth = '</a> </div> <a href="'
-var sixth = '" class="hoverMeta" tabindex="1"> <article class="post"> <div class="meta"> <div class="details"> <div class="time">'
-var seventh = '</div> </div> </div> <img src="'
-var eigth = '" alt="'
-var ninth = '"/> </article> </a> </div>'
+var first = '<div class="singlePost"><div class="summary">';
+var second = '</div> <div class="permalinkLinks"><a href="';
+var third = '" class="highresMeta">Hi-res</a> <a href="';
+var fourth = '" class="notesMeta">';
+var fifth = '</a> </div> <a href="';
+var sixth = '" class="hoverMeta" tabindex="1"> <article class="post"> <div class="meta"> <div class="details"> <div class="time">';
+var seventh = '</div> </div> </div> <img src="';
+var eigth = '" alt="';
+var ninth = '"/> </article> </a> </div>';
 
 getmore = function(){
+	if (stop) {
+		return;
+	}
 	jQuery.getJSON( '/dash.do', 
 		{
 			off: current
@@ -19,15 +23,20 @@ getmore = function(){
 		function(data) {
 			current = current + 40;
 
+			var done = true;
 			jQuery.each(data, function(i){
 
 				if(( ids[data[i].id] == true) == false ) {
 					ids[data[i].id] = true;
+					done = false;
 
 					jQuery(first.concat(data[i].caption, second, data[i].hires, third, data[i].perma, '#notes', fourth, data[i].numnotes, fifth, data[i].perma, sixth, data[i].date, seventh, data[i].img, eigth, data[i].caption, ninth)).appendTo("#content");
 				}
 
 		});
+			if(done) {
+				stop = true;
+			}
 	});
 };
 
